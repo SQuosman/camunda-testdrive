93be9e1b100a4c5c3f00ac2bd4a9d43a0d33a884
Stefan Quosbarth <s.quosbarth@gmail.com>
2016-03-11 14:01:55 +0100

    Deploy-Ready
    Signed-off-by: Stefan Quosbarth <s.quosbarth@gmail.com>

--- lab/src/test/java/de/sq/camunda/lab/InMemoryH2Test.java
+++ lab/src/test/java/de/sq/camunda/lab/InMemoryH2Test.java
@@ -2,6 +2,8 @@ package de.sq.camunda.lab;
 
 import org.apache.ibatis.logging.LogFactory;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.junit.After;
 import org.junit.Before;
@@ -11,15 +13,18 @@ import org.junit.Test;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.camunda.bpm.consulting.process_test_coverage.ProcessTestCoverage;
-//change made
+
 /**
  * Test case starting an in-memory database-backed Process Engine.
  */
 public class InMemoryH2Test {
 
   @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+  public ProcessEngineRule processEngineRule = new ProcessEngineRule();
 
   private static final String PROCESS_DEFINITION_KEY = "lab";
 
@@ -29,7 +34,7 @@ public class InMemoryH2Test {
 
   @Before
   public void setup() {
-    init(rule.getProcessEngine());
+    init(processEngineRule.getProcessEngine());
   }
 
   /**
@@ -44,17 +49,27 @@ public class InMemoryH2Test {
   @Test
   @Deployment(resources = "process.bpmn")
   public void testHappyPath() {
-	  //ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+	  //process variables
+	  Map<String, Object> variables = new HashMap<String, Object>();
+	  variables.put("content", "SQ / Test not approved (please don't tweet this!)");
+	  variables.put("approved", Boolean.FALSE);
 	  
-	  // Now: Drive the process by API and assert correct behavior by camunda-bpm-assert
+	  ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY, variables);
 	  
-	  // To generate the coverage report for a single tests add this line as the last line of your test method:
-	  //ProcessTestCoverage.calculate(processInstance, rule.getProcessEngine());
+    Task task = processEngineRule.getTaskService().createTaskQuery().taskAssignee("demo").singleResult();
+    processEngine().getTaskService().complete(task.getId(), variables);
+	  
+    // assert that all activities where passed
+    assertThat(processInstance) //
+    	.isEnded() //
+    	.hasPassed("end_event_tweet_handled", "gateway_approved", "gateway_join", "start_event_new_tweet", "user_task_review_tweet");
+
+    ProcessTestCoverage.calculate(processInstance, processEngineRule.getProcessEngine());
   }
 
   @After
   public void calculateCoverageForAllTests() throws Exception {
-    ProcessTestCoverage.calculate(rule.getProcessEngine());
+    ProcessTestCoverage.calculate(processEngineRule.getProcessEngine());
   }  
 
 }
